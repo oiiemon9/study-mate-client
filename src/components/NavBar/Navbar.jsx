230774d3dd6c4b026e2b17a88e92e34c71c38843
oiiemon9 <oiiemon9@gmail.com>
2025-11-08 21:05:46 +0600

    login and registration page design

---
+++ src/components/NavBar/Navbar.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router';
+
+const Navbar = () => {
+  const navlinks = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/">Find Partners</NavLink>
+      </li>
+      <li>
+        <NavLink to="/">Create Partner Profile</NavLink>
+      </li>
+      <li>
+        <NavLink to="/">My Connections</NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {' '}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{' '}
+            </svg>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {navlinks}
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{navlinks}</ul>
+      </div>
+      <div className="navbar-end">
+        <div className="flex gap-1">
+          <Link to="/login" className="btn bg-green-500 rounded-2xl text-white">
+            Login
+          </Link>
+          <Link
+            to="/register"
+            className="btn bg-green-500 rounded-2xl text-white"
+          >
+            Register
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
