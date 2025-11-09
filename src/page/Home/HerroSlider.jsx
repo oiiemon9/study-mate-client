b6f1f0d2a0ff7f379156a7f2380883a3a040ab63
oiiemon9 <oiiemon9@gmail.com>
2025-11-09 13:13:44 +0600

    hero section responsive complete

--- src/page/Home/HerroSlider.jsx
+++ src/page/Home/HerroSlider.jsx
@@ -25,28 +25,28 @@ const HerroSlider = () => {
           clickable: true,
         }}
         modules={[Autoplay, EffectFade, Navigation, Pagination]}
-        className="mySwiper h-screen"
+        className="mySwiper h-[300px] md:h-[360px] lg:h-[500px] xl:h-screen"
       >
-        <SwiperSlide className="bg-[url('https://i.ibb.co.com/5Ww60yJz/2149285424.jpg')] bg-cover bg-no-repeat h-full  ">
+        <SwiperSlide className="bg-[url('https://i.ibb.co.com/5Ww60yJz/2149285424.jpg')] bg-cover bg-no-repeat h-full">
           <div className="h-full bg-[linear-gradient(90deg,rgba(0,0,0,0.75)_0%,rgba(0,0,0,0)_51%)]">
             <div className="max-w-[1440px] mx-auto px-6 flex flex-col h-full justify-center text-white ">
-              <div className="ms-20 space-y-6">
-                <p className="text-sm uppercase tracking-wider text-green-400 font-semibold">
+              <div className="lg:ms-20 space-y-3 md:space-y-6">
+                <p className="text-xs md:text-sm uppercase tracking-wider text-green-400 font-semibold">
                   Find Your Study Buddy
                 </p>
-                <h1 className="text-5xl font-extrabold drop-shadow-lg">
+                <h1 className="text-xl md:text-3xl lg:text-5xl font-extrabold drop-shadow-lg">
                   Study Smarter,{' '}
                   <span className="text-green-400">Together.</span>
                 </h1>
 
-                <p className="max-w-xl text-lg  text-gray-100/90 drop-shadow-md">
+                <p className="max-w-2xs md:max-w-xl text-xs md:text-lg  text-gray-100/90 drop-shadow-md">
                   Connect with passionate learners who share your study goals,
                   match your schedule, and motivate you to achieve more —
                   together.
                 </p>
 
                 <div>
-                  <button className="mt-4 border border-green-600 hover:bg-green-600  text-green-600 hover:text-white px-6 py-3 rounded-xl font-semibold shadow-md transition duration-300 cursor-pointer">
+                  <button className="mt-4 border border-green-600 hover:bg-green-600  text-green-600 hover:text-white text-sm md:text-base px-3 py-1 md:px-6 md:py-3 rounded-xl font-semibold shadow-md transition duration-300 cursor-pointer">
                     Find a Study Mate
                   </button>
                 </div>
@@ -54,26 +54,26 @@ const HerroSlider = () => {
             </div>
           </div>
         </SwiperSlide>
-        <SwiperSlide className="bg-[url('https://i.ibb.co.com/1JbXVrv8/2884.jpg')] bg-cover bg-no-repeat h-full  ">
+        <SwiperSlide className="bg-[url('https://i.ibb.co.com/1JbXVrv8/2884.jpg')] bg-cover bg-no-repeat h-full">
           <div className="h-full bg-[linear-gradient(90deg,rgba(0,0,0,0.75)_0%,rgba(0,0,0,0)_51%)]">
             <div className="max-w-[1440px] mx-auto px-6 flex flex-col h-full justify-center text-white ">
-              <div className="ms-20 space-y-6">
-                <p className="text-sm uppercase tracking-wider text-green-400 font-semibold">
+              <div className="lg:ms-20 space-y-3 md:space-y-6">
+                <p className="text-xs md:text-sm uppercase tracking-wider text-green-400 font-semibold">
                   Find Your Study Buddy
                 </p>
-                <h1 className="text-5xl font-extrabold drop-shadow-lg">
-                  Turn Study Time into
+                <h1 className="text-xl md:text-3xl lg:text-5xl font-extrabold drop-shadow-lg">
+                  Turn Study Time into{' '}
                   <span className="text-green-400">Team Time.</span>
                 </h1>
 
-                <p className="max-w-xl text-lg  text-gray-100/90 drop-shadow-md">
+                <p className="max-w-2xs md:max-w-xl text-xs md:text-lg  text-gray-100/90 drop-shadow-md">
                   Find learners who share your goals, study habits, and dream
                   big just like you. Build connections that make studying fun
                   and productive.
                 </p>
 
                 <div>
-                  <button className="mt-4 border border-green-600 hover:bg-green-600  text-green-600 hover:text-white px-6 py-3 rounded-xl font-semibold shadow-md transition duration-300 cursor-pointer">
+                  <button className="mt-4 border border-green-600 hover:bg-green-600  text-green-600 hover:text-white text-sm md:text-base px-3 py-1 md:px-6 md:py-3 rounded-xl font-semibold shadow-md transition duration-300 cursor-pointer">
                     Create Your Profile
                   </button>
                 </div>
@@ -81,26 +81,26 @@ const HerroSlider = () => {
             </div>
           </div>
         </SwiperSlide>
-        <SwiperSlide className="bg-[url('https://i.ibb.co.com/CKR6ZTyS/pexels-artempodrez-4492126.jpg')] bg-cover bg-no-repeat h-full  ">
+        <SwiperSlide className="bg-[url('https://i.ibb.co.com/CKR6ZTyS/pexels-artempodrez-4492126.jpg')] bg-cover bg-no-repeat h-full">
           <div className="h-full bg-[linear-gradient(90deg,rgba(0,0,0,0.75)_0%,rgba(0,0,0,0)_51%)]">
             <div className="max-w-[1440px] mx-auto px-6 flex flex-col h-full justify-center text-white ">
-              <div className="ms-20 space-y-6">
-                <p className="text-sm uppercase tracking-wider text-green-400 font-semibold">
+              <div className="lg:ms-20 space-y-3 md:space-y-6">
+                <p className="text-xs md:text-sm uppercase tracking-wider text-green-400 font-semibold">
                   Find Your Study Buddy
                 </p>
-                <h1 className="text-5xl font-extrabold drop-shadow-lg">
+                <h1 className="text-xl md:text-3xl lg:text-5xl font-extrabold drop-shadow-lg">
                   Study Smarter,{' '}
-                  <span className="text-green-400">Great Company</span>
+                  <span className="text-green-400">Great Company.</span>
                 </h1>
 
-                <p className="max-w-xl text-lg  text-gray-100/90 drop-shadow-md">
+                <p className="max-w-2xs md:max-w-xl text-xs md:text-lg  text-gray-100/90 drop-shadow-md">
                   Connect with dedicated students who align with your subjects,
                   time, and learning style — and grow together through shared
                   goals.
                 </p>
 
                 <div>
-                  <button className="mt-4 border border-green-600 hover:bg-green-600  text-green-600 hover:text-white px-6 py-3 rounded-xl font-semibold shadow-md transition duration-300 cursor-pointer">
+                  <button className="mt-4 border border-green-600 hover:bg-green-600  text-green-600 hover:text-white text-sm md:text-base px-3 py-1 md:px-6 md:py-3 rounded-xl font-semibold shadow-md transition duration-300 cursor-pointer">
                     Find a Study Mate
                   </button>
                 </div>
