647279101964907a16cf2dbebc474ceb0d2428f7
oiiemon9 <oiiemon9@gmail.com>
2025-11-11 20:31:57 +0600

    login , register form padding edit

--- src/page/Register/Register.jsx
+++ src/page/Register/Register.jsx
@@ -78,154 +78,156 @@ const Register = () => {
     }
   };
   return (
-    <div className=" bg-white dark:bg-base-200 border border-gray-200 dark:border-gray-700 rounded-xl shadow-2xs max-w-lg mx-auto  mb-24 mt-32 px-2">
-      <Helmet>
-        <title>Register | Study Mate</title>
-        <meta
-          name="description"
-          content="Find your perfect study partner and achieve your goals together!"
-        />
-        <meta
-          name="keywords"
-          content="study partner, learning, education, connect"
-        />
-      </Helmet>
-      <div className="p-4 sm:p-7">
-        <div className="text-center">
-          <h1 className="block text-2xl font-bold text-gray-800 dark:text-white">
-            Register
-          </h1>
-          <p className="mt-2 text-sm text-gray-600">
-            Already have an account?{' '}
-            <Link to="/login" className="text-green-600 hover:underline">
-              Log in here
-            </Link>
-          </p>
-        </div>
-
-        <div className="mt-5">
-          <button
-            onClick={handelGoogleLogin}
-            type="button"
-            className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 dark:border-gray-700 bg-white dark:bg-base-100 text-gray-800 dark:text-white shadow-2xs hover:bg-gray-50 dark:hover:bg-base-200 focus:outline-hidden  cursor-pointer"
-          >
-            <svg
-              className="w-4 h-auto"
-              width="46"
-              height="47"
-              viewBox="0 0 46 47"
-              fill="none"
-            >
-              <path
-                d="M46 24.0287C46 22.09 45.8533 20.68 45.5013 19.2112H23.4694V27.9356H36.4069C36.1429 30.1094 34.7347 33.37 31.5957 35.5731L31.5663 35.8669L38.5191 41.2719L38.9885 41.3306C43.4477 37.2181 46 31.1669 46 24.0287Z"
-                fill="#4285F4"
-              />
-              <path
-                d="M23.4694 47C29.8061 47 35.1161 44.9144 39.0179 41.3012L31.625 35.5437C29.6301 36.9244 26.9898 37.8937 23.4987 37.8937C17.2793 37.8937 12.0281 33.7812 10.1505 28.1412L9.88649 28.1706L2.61097 33.7812L2.52296 34.0456C6.36608 41.7125 14.287 47 23.4694 47Z"
-                fill="#34A853"
-              />
-              <path
-                d="M10.1212 28.1413C9.62245 26.6725 9.32908 25.1156 9.32908 23.5C9.32908 21.8844 9.62245 20.3275 10.0918 18.8588V18.5356L2.75765 12.8369L2.52296 12.9544C0.909439 16.1269 0 19.7106 0 23.5C0 27.2894 0.909439 30.8731 2.49362 34.0456L10.1212 28.1413Z"
-                fill="#FBBC05"
-              />
-              <path
-                d="M23.4694 9.07688C27.8699 9.07688 30.8622 10.9863 32.5344 12.5725L39.1645 6.11C35.0867 2.32063 29.8061 0 23.4694 0C14.287 0 6.36607 5.2875 2.49362 12.9544L10.0918 18.8588C11.9987 13.1894 17.25 9.07688 23.4694 9.07688Z"
-                fill="#EB4335"
-              />
-            </svg>
-            Sign in with Google
-          </button>
-
-          <div className="py-3 flex items-center text-xs text-gray-400 uppercase before:flex-1 before:border-t before:border-gray-200 before:me-6 after:flex-1 after:border-t after:border-gray-200 dark:after:border-gray-700 dark:before:border-gray-700 after:ms-6">
-            Or
+    <div className="max-w-lg mx-auto  mb-24 mt-32 px-2">
+      <div className=" bg-white dark:bg-base-200 border border-gray-200 dark:border-gray-700 rounded-xl shadow-2xs ">
+        <Helmet>
+          <title>Register | Study Mate</title>
+          <meta
+            name="description"
+            content="Find your perfect study partner and achieve your goals together!"
+          />
+          <meta
+            name="keywords"
+            content="study partner, learning, education, connect"
+          />
+        </Helmet>
+        <div className="p-4 sm:p-7">
+          <div className="text-center">
+            <h1 className="block text-2xl font-bold text-gray-800 dark:text-white">
+              Register
+            </h1>
+            <p className="mt-2 text-sm text-gray-600">
+              Already have an account?{' '}
+              <Link to="/login" className="text-green-600 hover:underline">
+                Log in here
+              </Link>
+            </p>
           </div>
 
-          <form onSubmit={handelRegister}>
-            <div className="grid gap-y-4">
-              <div>
-                <label htmlFor="name" className="block text-sm mb-2">
-                  Name
-                </label>
-                <div className="relative">
-                  <input
-                    type="text"
-                    name="name"
-                    className="py-2.5 sm:py-3 px-4 block w-full rounded-lg sm:text-sm outline outline-gray-300 dark:outline-gray-700 focus:outline-green-600"
-                    placeholder="Your name"
-                    required
-                  />
-                </div>
-              </div>
-              <div>
-                <label htmlFor="Photo" className="block text-sm mb-2">
-                  Photo Url
-                </label>
-                <div className="relative">
-                  <input
-                    type="text"
-                    name="photo"
-                    className="py-2.5 sm:py-3 px-4 block w-full  rounded-lg sm:text-sm outline outline-gray-300 dark:outline-gray-700 focus:outline-green-600"
-                    placeholder="Your Photo URL"
-                    required
-                  />
-                </div>
-              </div>
-              <div>
-                <label htmlFor="email" className="block text-sm mb-2">
-                  Email address
-                </label>
-                <div className="relative">
-                  <input
-                    type="email"
-                    name="email"
-                    className="py-2.5 sm:py-3 px-4 block w-full rounded-lg sm:text-sm outline outline-gray-300 dark:outline-gray-700 focus:outline-green-600"
-                    placeholder="Your Email"
-                    required
-                  />
-                </div>
-              </div>
+          <div className="mt-5">
+            <button
+              onClick={handelGoogleLogin}
+              type="button"
+              className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 dark:border-gray-700 bg-white dark:bg-base-100 text-gray-800 dark:text-white shadow-2xs hover:bg-gray-50 dark:hover:bg-base-200 focus:outline-hidden  cursor-pointer"
+            >
+              <svg
+                className="w-4 h-auto"
+                width="46"
+                height="47"
+                viewBox="0 0 46 47"
+                fill="none"
+              >
+                <path
+                  d="M46 24.0287C46 22.09 45.8533 20.68 45.5013 19.2112H23.4694V27.9356H36.4069C36.1429 30.1094 34.7347 33.37 31.5957 35.5731L31.5663 35.8669L38.5191 41.2719L38.9885 41.3306C43.4477 37.2181 46 31.1669 46 24.0287Z"
+                  fill="#4285F4"
+                />
+                <path
+                  d="M23.4694 47C29.8061 47 35.1161 44.9144 39.0179 41.3012L31.625 35.5437C29.6301 36.9244 26.9898 37.8937 23.4987 37.8937C17.2793 37.8937 12.0281 33.7812 10.1505 28.1412L9.88649 28.1706L2.61097 33.7812L2.52296 34.0456C6.36608 41.7125 14.287 47 23.4694 47Z"
+                  fill="#34A853"
+                />
+                <path
+                  d="M10.1212 28.1413C9.62245 26.6725 9.32908 25.1156 9.32908 23.5C9.32908 21.8844 9.62245 20.3275 10.0918 18.8588V18.5356L2.75765 12.8369L2.52296 12.9544C0.909439 16.1269 0 19.7106 0 23.5C0 27.2894 0.909439 30.8731 2.49362 34.0456L10.1212 28.1413Z"
+                  fill="#FBBC05"
+                />
+                <path
+                  d="M23.4694 9.07688C27.8699 9.07688 30.8622 10.9863 32.5344 12.5725L39.1645 6.11C35.0867 2.32063 29.8061 0 23.4694 0C14.287 0 6.36607 5.2875 2.49362 12.9544L10.0918 18.8588C11.9987 13.1894 17.25 9.07688 23.4694 9.07688Z"
+                  fill="#EB4335"
+                />
+              </svg>
+              Sign in with Google
+            </button>
 
-              <div>
-                <div className="flex flex-wrap justify-between items-center gap-2">
-                  <label htmlFor="password" className="block text-sm mb-2">
-                    Password
-                  </label>
-                </div>
+            <div className="py-3 flex items-center text-xs text-gray-400 uppercase before:flex-1 before:border-t before:border-gray-200 before:me-6 after:flex-1 after:border-t after:border-gray-200 dark:after:border-gray-700 dark:before:border-gray-700 after:ms-6">
+              Or
+            </div>
+
+            <form onSubmit={handelRegister}>
+              <div className="grid gap-y-4">
                 <div>
+                  <label htmlFor="name" className="block text-sm mb-2">
+                    Name
+                  </label>
                   <div className="relative">
                     <input
-                      type={showPass ? 'text' : 'password'}
-                      name="password"
+                      type="text"
+                      name="name"
                       className="py-2.5 sm:py-3 px-4 block w-full rounded-lg sm:text-sm outline outline-gray-300 dark:outline-gray-700 focus:outline-green-600"
-                      placeholder="Password"
+                      placeholder="Your name"
                       required
                     />
-                    <button
-                      type="button"
-                      onClick={() => setShowPass(!showPass)}
-                      className="absolute top-0 bottom-0 right-5 cursor-pointer"
-                    >
-                      {showPass ? <FaEyeSlash /> : <IoEyeSharp />}
-                    </button>
                   </div>
                 </div>
-              </div>
+                <div>
+                  <label htmlFor="Photo" className="block text-sm mb-2">
+                    Photo Url
+                  </label>
+                  <div className="relative">
+                    <input
+                      type="text"
+                      name="photo"
+                      className="py-2.5 sm:py-3 px-4 block w-full  rounded-lg sm:text-sm outline outline-gray-300 dark:outline-gray-700 focus:outline-green-600"
+                      placeholder="Your Photo URL"
+                      required
+                    />
+                  </div>
+                </div>
+                <div>
+                  <label htmlFor="email" className="block text-sm mb-2">
+                    Email address
+                  </label>
+                  <div className="relative">
+                    <input
+                      type="email"
+                      name="email"
+                      className="py-2.5 sm:py-3 px-4 block w-full rounded-lg sm:text-sm outline outline-gray-300 dark:outline-gray-700 focus:outline-green-600"
+                      placeholder="Your Email"
+                      required
+                    />
+                  </div>
+                </div>
 
-              <button
-                type="submit"
-                className="w-full py-3 px-4  text-sm font-medium rounded-lg border  border-green-600 bg-green-100  dark:bg-base-200 cursor-pointer"
-              >
-                {loading && (
-                  <span className="loading loading-spinner text-primary"></span>
-                )}{' '}
-                Register
-              </button>
-            </div>
-            <div className="mt-2">
-              {error && <p className="text-red-600">{error}</p>}
-            </div>
-          </form>
-          {/* End Form */}
+                <div>
+                  <div className="flex flex-wrap justify-between items-center gap-2">
+                    <label htmlFor="password" className="block text-sm mb-2">
+                      Password
+                    </label>
+                  </div>
+                  <div>
+                    <div className="relative">
+                      <input
+                        type={showPass ? 'text' : 'password'}
+                        name="password"
+                        className="py-2.5 sm:py-3 px-4 block w-full rounded-lg sm:text-sm outline outline-gray-300 dark:outline-gray-700 focus:outline-green-600"
+                        placeholder="Password"
+                        required
+                      />
+                      <button
+                        type="button"
+                        onClick={() => setShowPass(!showPass)}
+                        className="absolute top-0 bottom-0 right-5 cursor-pointer"
+                      >
+                        {showPass ? <FaEyeSlash /> : <IoEyeSharp />}
+                      </button>
+                    </div>
+                  </div>
+                </div>
+
+                <button
+                  type="submit"
+                  className="w-full py-3 px-4  text-sm font-medium rounded-lg border  border-green-600 bg-green-100  dark:bg-base-200 cursor-pointer"
+                >
+                  {loading && (
+                    <span className="loading loading-spinner text-primary"></span>
+                  )}{' '}
+                  Register
+                </button>
+              </div>
+              <div className="mt-2">
+                {error && <p className="text-red-600">{error}</p>}
+              </div>
+            </form>
+            {/* End Form */}
+          </div>
         </div>
       </div>
     </div>
